5fc844394c23d7107c6923b61cdfe47903bbb635
Bhavnish Arora <bhavnisharora786@gmail.com>
2024-05-20 18:42:24 +0530

    changes made in themeSwitcher

--- src/components/hooks/useThemeSwicther.js
+++ src/components/hooks/useThemeSwicther.js
@@ -1,3 +1,4 @@
+import { Elsie_Swash_Caps } from "next/font/google";
 import react, { useEffect, useState } from "react";
 
 const useThemeSwitcher = () => {
@@ -7,6 +8,34 @@ const useThemeSwitcher = () => {
   useEffect(() => {
     const mediaQuery = window.matchMedia(preferDarkQuery);
     const userPref = window.localStorage.getItem("theme");
+
+    const handleChange = () => {
+      if (userPref) {
+        let check = userPref === "dark" ? "dark" : "light";
+        setMode(check);
+
+        if (check == "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      } else {
+        let check = mediaQuery.matches ? "dark" : "light";
+        setMode(check);
+
+        if (check == "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () =>{
+        mediaQuery.removeEventListener("change",handleChange);
+    }
   }, []);
   return <div>hello</div>;
 };
